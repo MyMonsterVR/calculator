7fdf281c61f67c9a9d521b6fb9cf02805c97302b
Rasmus Høllund Kristensen <31127327+MyMonsterVR@users.noreply.github.com>
2021-09-15 07:55:55 +0200

    Added more features

--- assets/cal.js
+++ assets/cal.js
@@ -4,37 +4,65 @@ function parse(str) {
   return Function(`'use strict'; return (${str})`)()
 }
 
-
 // selve regningen
 var mathString = "";
+var equals = false;
 const keys = document.querySelector(".calculator-keys");
 
 const display = document.querySelector(".calculator-screen");
 display.value = "0";
+
+var sintancos;
 
 keys.addEventListener("click", (event) => {
 	const {target} = event;
 	const {value} = target;
 	if (!target.matches("button")) {
 		return;
-	}
+  }
 
 	switch (value) {
 		case "=":
 			// "Undefined" text prevention
 			if (mathString == "") return (display.value = "0");
-			if (mathString.includes("√")) {
+
+      if (mathString.includes("√")) {
 				display.value = Math.sqrt(mathString.replace("√", ""));
 				return Clear();
 			}
-			if (mathString.includes("^")) {
+
+			else if (mathString.includes("^")) {
 				display.value = Math.pow(
 					mathString.split("^")[0],
 					mathString.split("^")[1].replace("^", "")
 				);
 				return Clear();
 			}
+
+      else if (mathString.includes("cos")) {
+        result = Math.cos(mathString.replace("cos(", "").replace(")", "").replace("cos", "")).toFixed(4)
+        display.value = Math.cos(mathString.replace("cos(", "").replace(")", "").replace("cos", ""));
+        historyUpdate()
+        return Clear();
+      }
+
+      else if (mathString.includes("tan")) {
+        result = Math.tan(mathString.replace("tan(", "").replace(")", "").replace("tan", "")).toFixed(4)
+        display.value = Math.tan(mathString.replace("tan(", "").replace(")", "").replace("tan", ""));
+        historyUpdate()
+        return Clear();
+      }
+
+      else if (mathString.includes("sin")) {
+        result = Math.sin(mathString.replace("sin(", "").replace(")", "").replace("sin", "")).toFixed(4)
+        display.value = Math.sin(mathString.replace("sin(", "").replace(")", "").replace("sin", ""));
+        historyUpdate()
+        return Clear();
+      }
+
 			// else continue
+			equals = true
+      console.log(equals)
 			updateDisplayNumber();
 			historyUpdate();
 			Clear();
@@ -47,13 +75,28 @@ keys.addEventListener("click", (event) => {
 		//clear button
 		case "all-clear":
 			Clear();
-			updateDisplayNumber();
-			display.value = "0";
-			break;
+			mathString = "";
+			display.value = "0"
+			break
 
+    case "pi":
+      mathString += "3.14"
+      display.value = mathString;
+      break
+    case "cos":
+      mathString += "cos("
+      break
+    case "sin":
+      mathString += "sin("
+      break
+    case "tan":
+      mathString += "tan("
+      break
 		//else do this
 		default:
+      equals = false;
 			mathString += value;
+      if (mathString.includes("cos(") || mathString.includes("sin(") || mathString.includes("tan(") && sintancos) mathString  += ")"
 			display.value = mathString;
 			break;
 	}
@@ -76,29 +119,36 @@ document.addEventListener("keydown", function (event) {
 					return Clear();
 				}
 				// else continue
+				equals = true
 				updateDisplayNumber();
 				historyUpdate();
 				Clear();
 			break
+		case "Backspace":
+			mathString = mathString.substring(0, mathString.length - 1)
+			if(mathString == "") mathString = 0
+			display.value = mathString
+			break
+
 		case ".":
 				mathString += "."
 				display.value = mathString
 			break
 		default:
 			if(isText) {
+				if(mathString == "0") mathString = ""
 				mathString += event.key
 				display.value = mathString
 			}
 			break
 	}
-
 });
 
 // Display opdatering
 function updateDisplayNumber() {
-	checkString = parse(mathString);
-	result = checkString;
-	display.value = parse(mathString);
+	result = parse(mathString);
+  if(mathString.includes(".") && equals) return display.value = result.toFixed(2);
+  display.value = result
 }
 
 // Clear funktion
@@ -108,6 +158,32 @@ function Clear() {
 }
 
 // CONSOLE MODE!
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+   if (localStorage.getItem('theme') === 'theme-orange'){
+       setTheme('theme-red');
+   } else {
+       setTheme('theme-orange');
+   }
+}
+
+// Theme on load
+(function () {
+   if (localStorage.getItem('theme') === 'theme-orange') {
+       setTheme('theme-orange');
+   } else {
+       setTheme('theme-red');
+   }
+})();
+
+// Delay
 
 function delay(delayInms) {
 	return new Promise((resolve) => {
@@ -127,10 +203,17 @@ let currentWRTL = -1;
 var input = document.getElementById("myInput");
 input.addEventListener("keyup", function (event) {
 	if (event.keyCode === 13) {
-		result = parse(input.value);
-		document.getElementById("consoleAnswer").style.display = "block";
-		document.getElementById("consoleAnswer").innerHTML = result;
-		historyUpdate();
+		if(input.value == "help") {
+			document.getElementById("consoleAnswer").style.display = "block";
+			document.getElementById("consoleAnswer").innerHTML = "type &ldquo;<strong>mode</strong>&ldquo; To Change Theme";
+		}
+
+		if(input.value == "mode") {
+			document.getElementById("consoleAnswer").style.display = "block";
+			document.getElementById("consoleAnswer").innerHTML = "Color changed";
+			toggleTheme()
+		}
+
 		return Reset();
 	}
 });
@@ -159,9 +242,10 @@ function historyShow() {
 function historyUpdate() {
 	var historyIdList = document.getElementsByName("historyIds");
 
+  // Infinite loop
 	if (currentWRTL >= 9) currentWRTL = -1;
 	currentWRTL++;
-
+  // læser alle elementer i gennem
 	historyIdList.forEach(function (elem) {
 		if (!historyIdList[currentWRTL] == "No Result") {
 			return;
